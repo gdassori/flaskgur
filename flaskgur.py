a4b7cf4e0a0dc97494549c6260b1417d367fed1d
Guido Dassori <guido.dassori@gmail.com>
2016-04-10 16:15:45 +0200

    - ported to python3 - requirements.txt - added base_path option - database automatic initialization if sqlite file not found - separated upload endpoint from index endpoint, with the purpose of protecting /upload with htpasswd

--- flaskgur.py
+++ flaskgur.py
@@ -1,68 +1,63 @@
+import binascii
+import hashlib
 from flask import Flask, request, g, redirect, url_for, abort, render_template, send_from_directory
-from werkzeug import secure_filename
-from hashlib import md5
 from PIL import Image
 import sqlite3
 import os
 import time
 
 DEBUG              = True
-BASE_DIR           = '/home/guido/dev/flaskgur/'
+BASE_DIR           = '/home/guido/Data/flaskgur/'
 UPLOAD_DIR         = BASE_DIR + 'pics'
 DATABASE           = BASE_DIR + 'flaskgur.db'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+BASE_PATH          = '/flaskgur/'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-#Init db
+
 def init_db():
-    try:
-        get_last_pics()
-        return 'already initialized'
-    except:
-        with app.app_context():
-            db = connect_db()
-            with app.open_resource('schema.sql', mode='r') as f:
-                db.cursor().executescript(f.read())
-            db.commit()
-        return 'done'
+    with app.app_context():
+        db = connect_db()
+        with app.open_resource(BASE_DIR+'schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 # Make sure extension is in the ALLOWD_EXTENSIONS set
 def check_extension(extension):
-	return extension in ALLOWED_EXTENSIONS
+    return extension in ALLOWED_EXTENSIONS
 
 def connect_db():
-  try:
-    return sqlite3.connect(app.config['DATABASE'])
-  except:
-    with open(DATABASE, 'wb') as f:
-      f.write('')
     return sqlite3.connect(app.config['DATABASE'])
 
 # Return a list of the last 25 uploaded images	
 def get_last_pics():
-	cur = g.db.execute('select filename from pics order by id desc limit 25')
-	filenames = [row[0] for row in cur.fetchall()]
-	return filenames
+    cur = g.db.execute('select filename from pics order by id desc limit 25')
+    filenames = [row[0] for row in cur.fetchall()]
+    return filenames
 
 # Insert filename into database	
 def add_pic(filename):
-	g.db.execute('insert into pics (filename) values (?)', [filename])
-	g.db.commit()
+    g.db.execute('insert into pics (filename) values (?)', [filename])
+    g.db.commit()
 
 # Generate thumbnail image
 def gen_thumbnail(filename):
-	height = width = 200
-	original = Image.open(os.path.join(app.config['UPLOAD_DIR'], filename))
-	thumbnail = original.resize((width, height), Image.ANTIALIAS)
-	thumbnail.save(os.path.join(app.config['UPLOAD_DIR'], 'thumb_'+filename))
+    height = width = 200
+    original = Image.open(os.path.join(app.config['UPLOAD_DIR'], filename))
+    thumbnail = original.resize((width, height), Image.ANTIALIAS)
+    thumbnail.save(os.path.join(app.config['UPLOAD_DIR'], 'thumb_'+filename))
 	
 # Taken from flask example app
 @app.before_request
 def before_request():
-    g.db = connect_db()
-    
+    try:
+       g.db = connect_db()
+    except sqlite3.OperationalError:
+        init_db()
+        g.db = connect_db()
+
 # Taken from flask example app
 @app.teardown_request
 def teardown_request(exception):
@@ -72,41 +67,45 @@ def teardown_request(exception):
         
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html', basepath=BASE_PATH), 404
 
-@app.route('/flaskgur', methods=['GET','POST'])
+@app.route(BASE_PATH, )
+def index():
+    return render_template('index.html', pics=get_last_pics(), basepath=BASE_PATH)
+
+@app.route(BASE_PATH + 'upload', methods=['POST'])
 def upload_pic():
-	if request.method == 'POST':
-		file = request.files['file']
-		try:
-			extension = file.filename.rsplit('.', 1)[1].lower()
-		except IndexError, e:
-			abort(404)
-		if file and check_extension(extension):
-			# Salt and hash the file contents
-			filename = md5(file.read() + str(round(time.time() * 1000))).hexdigest() + '.' + extension
-			file.seek(0) # Move cursor back to beginning so we can write to disk
-			file.save(os.path.join(app.config['UPLOAD_DIR'], filename))
-			add_pic(filename)
-			gen_thumbnail(filename)
-			return redirect(url_for('show_pic', filename=filename))
-		else: # Bad file extension
-			abort(404)
-	else:
-		return render_template('upload.html', pics=get_last_pics())
+    file = request.files['file']
+    try:
+        extension = file.filename.rsplit('.', 1)[1].lower()
+        if file and check_extension(extension):
+            # Salt and hash the file contents
+            filename = '{}.{}'.format(hashlib.md5('{}{}'.format(str(binascii.hexlify(file.read())),
+                                                                str(int(time.time()))).encode()).hexdigest(),
+                                      extension)
+            file.seek(0) # Move cursor back to beginning so we can write to disk
+            file.save(os.path.join(app.config['UPLOAD_DIR'], filename))
+            add_pic(filename)
+            gen_thumbnail(filename)
+            return redirect(url_for('show_pic', filename=filename, basepath=BASE_PATH))
+        else: # Bad file extension
+            abort(404)
+    except IndexError:
+        abort(404)
 
-@app.route('{}/init'.format(PREFIX))
-def init():
-    return init_db()
-
-@app.route('/show')
+@app.route(BASE_PATH + 'show')
 def show_pic():
-	filename = request.args.get('filename', '')
-	return render_template('upload.html', filename=filename)
+    filename = request.args.get('filename', '')
+    return render_template('index.html', filename=filename, basepath=BASE_PATH)
 
-@app.route('/pics/<filename>')
+@app.route(BASE_PATH + 'pics/<filename>')
 def return_pic(filename):
-	return send_from_directory(app.config['UPLOAD_DIR'], secure_filename(filename))
+    return send_from_directory(app.config['UPLOAD_DIR'], filename)
 	
 if __name__ == '__main__':
-	app.run(debug=DEBUG, host='0.0.0.0')
+    try:
+        with open(DATABASE, 'rb') as f:
+            f.readline()
+    except:
+        init_db()
+    app.run(debug=DEBUG, host='0.0.0.0')
